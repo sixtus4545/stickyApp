6faba3018cc08916f92adfc7829af3167743d18a
sixtus <sixtusiwuchukwu21@gmail.com>
2019-09-17 15:53:58 +0100

    production

---
+++ build/precache-manifest.ff05c10ab46e409fe1e9ca5852a0af88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77839b820cf05507faf3db8c379d41e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8e4298637bfac4ffcb3",
+    "url": "/static/css/main.8fb6dd2d.chunk.css"
+  },
+  {
+    "revision": "46ecf5d1647bc9d925b1",
+    "url": "/static/js/2.1b125461.chunk.js"
+  },
+  {
+    "revision": "a8e4298637bfac4ffcb3",
+    "url": "/static/js/main.80a5772a.chunk.js"
+  },
+  {
+    "revision": "0c1400ec95fcc8fa04a7",
+    "url": "/static/js/runtime~main.5134c318.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
